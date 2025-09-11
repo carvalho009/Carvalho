c412e9f3a55cc45b174424c8bb8ae1ddd1ff5b03
carvalho009 <gustavo.gcj09@gmail.com>
2025-09-11 09:22:57 -0300

    teste

--- lista.py
+++ lista.py
@@ -1,7 +1,10 @@
 while True:
-    fruta = int(input("Coloque um número de 0 a 5: "))
-    frutas = ['Banana', 'Maça', 'Uva', 'Manga', 'Abacaxi']
-    print(frutas[fruta])
-    continua = input("Deseja continuar?[sim, nao]: ").lower()
-    if continua == 'nao':
-        break
+    try:
+        fruta = int(input("Coloque um número de 0 a 4: "))
+        frutas = ['Banana', 'Maça', 'Uva', 'Manga', 'Abacaxi']
+        print(frutas[fruta])
+        continua = input("Deseja continuar?[sim, nao]: ").lower()
+        if continua == 'nao':
+            break
+    except IndexError:
+        print("Erro de indice")
